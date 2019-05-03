885250b28e16fc7675a4a78ef425e7de716207f1
Kyle Rowan <karowan55@gmail.com>
2019-05-03 09:35:54 -0700

    Phase 1 complete

--- script.js
+++ script.js
@@ -5,7 +5,10 @@
     leftArrow: "left-arrow",
     rightArrow: "right-arrow",
     select: "select",
-    start: "start"
+    start: "start",
+    b: "b-button",
+    a: "a-button",
+    gamepad: "gamepad-outer"
   };
   let count = 0;
   document
@@ -40,4 +43,22 @@
     .addEventListener("click", function() {
       count = 0;
     });
+  document.getElementById(DOMstrings.b).addEventListener("click", function() {
+    count === 8 ? count++ : (count = 0);
+    console.log(count);
+  });
+  document.getElementById(DOMstrings.a).addEventListener("click", function() {
+    count === 9 ? konami() : (count = 0);
+    console.log(count);
+  });
+  function konami() {
+    let element = document.getElementById(DOMstrings.gamepad);
+    let parent = element.parentElement;
+    parent.removeChild(element);
+    let p = document.createElement("p");
+    let t = document.createTextNode("Welcome");
+    p.appendChild(t);
+    p.id = "welcome";
+    parent.appendChild(p);
+  }
 })();
